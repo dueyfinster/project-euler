7fa28fb94256bdfc97ee35a28aae1032a4154bcc
Neil Grogan <neil@ngrogan.com>
2016-04-10 18:41:07 +0100

    Added tests for problem 1

--- src/main/java/euler/Problem1.java
+++ src/main/java/euler/Problem1.java
@@ -2,10 +2,10 @@ package euler;
 
 public class Problem1 {
 	
-	public static int addMultiples(){
+	public static int addMultiples(int limit){
 		int answer = 0;
 		
-		for(int i=0; i<1000; i++){
+		for(int i=0; i<limit; i++){
 			if(i%15!=0 && i%3==0 || i%5==0){
 				answer=answer+i;
 			}
@@ -14,7 +14,7 @@ public class Problem1 {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Problem 1: " + addMultiples());
+		System.out.println("Problem 1: " + addMultiples(1000));
 	}
 
 }
--- src/test/java/euler/Test1.java
+++ src/test/java/euler/Test1.java
@@ -1,5 +1,29 @@
 package euler;
 
-public class Test1 {
+import static org.junit.Assert.assertEquals;
 
+
+import org.junit.Test;
+
+public class Test1 {
+	
+	@Test
+	public void test0(){
+		assertEquals(Problem1.addMultiples(0), 0);
+	}
+
+	@Test
+	public void test10(){
+		assertEquals(Problem1.addMultiples(10), 23);
+	}
+	
+	@Test
+	public void test100(){
+		assertEquals(Problem1.addMultiples(100), 2318);
+	}
+	
+	@Test
+	public void test1000(){
+		assertEquals(Problem1.addMultiples(1000), 233168);
+	}
 }
